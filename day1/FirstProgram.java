cbe399e216d884f287567c8c31e125c715ebef79
mat-hi <100179370+mat-hi@users.noreply.github.com>
2025-01-26 12:39:24 +0530

    Add files via upload
    day1

---
+++ day1/FirstProgram.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class FirstProgram {
+	
+	//main -> ctrl + Space
+	public static void main(String[] args) {
+		
+		//syso -> ctrl+ sPACE
+		System.out.println("My First Code");
+		
+		System.out.println("Welcome to Testleaf");
+		
+	}
+
+}
